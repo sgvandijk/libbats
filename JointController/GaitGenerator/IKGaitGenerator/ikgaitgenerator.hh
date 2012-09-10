c30ca891f01913f2b0611fd2fe44772e37810095
Sander van Dijk <sgvandijk@gmail.com>
2012-09-10 18:53:46 +0100

    - Start of IKGaitGenerator

---
+++ JointController/GaitGenerator/IKGaitGenerator/ikgaitgenerator.hh
@@ -0,0 +1,17 @@
+#ifndef BATS_IKGAITGENERATOR_HH
+#define BATS_IKGAITGENERATOR_HH
+
+#include "../gaitgenerator.hh"
+
+namespace bats
+{
+  class IKGaitGenerator : public Gaitgenerator
+  {
+  public:
+
+  private:
+
+  };
+}
+
+#endif
