a9265b4b7ac17bd21d24dc2de2101bed75f64863
Sander van Dijk <sgvandijk@gmail.com>
2013-02-27 19:05:28 +0000

    - Added conf.dtd - remove old util scripts - update info files

--- makelib.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-find . -name '*.hh' | egrep -v '(_sse)|(_fpu)' | perl -ne 'chomp; print "#include \""; print; print "\"\n";' > libbats.cc
-ccbuild build --compiler 'libtool --tag=CXX --mode=compile g++' libbats.cc --verbose --args "-O2 -DBATS_NODEBUG -DNODEBUG `xml2-config --cflags`"
-find . -name '*.lo' -exec libtool --tag=CXX --mode=link g++ -g -O0 -o libbats.la -rpath /usr/local/lib -lxml2 {} +
-
